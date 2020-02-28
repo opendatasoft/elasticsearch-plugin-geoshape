f96fd42b8ecdcc2c5a7d68283a819773bf498b35
Matthieu Riviere <matthieu.riviere@opendatasoft.com>
2020-02-28 14:37:08 +0100

    ScriptGeoSimplify now uses the same tolerance calculation as GeoShapeAggregator

--- src/main/java/org/opendatasoft/elasticsearch/plugin/GeoUtils.java
+++ src/main/java/org/opendatasoft/elasticsearch/plugin/GeoUtils.java
@@ -69,6 +69,16 @@ public class GeoUtils {
         return meter * 360 / (org.elasticsearch.common.geo.GeoUtils.EARTH_EQUATOR * Math.cos(Math.toRadians(latitude)));
     }
 
+    public static double getToleranceFromZoom(int zoom) {
+        /*
+        This is a simplified formula for
+        double meterByPixel = GeoUtils.getMeterByPixel(zoom, lat);
+        double tol = GeoUtils.getDecimalDegreeFromMeter(meterByPixel * pixelTolerance, lat);
+        when pixelTolerance == 1
+         */
+        return 360 / (256 * Math.pow(2, zoom));
+    }
+
 
     public static String exportWkbTo(BytesRef wkb, InternalGeoShape.OutputFormat output_format, GeoJsonWriter geoJsonWriter)
             throws ParseException {
