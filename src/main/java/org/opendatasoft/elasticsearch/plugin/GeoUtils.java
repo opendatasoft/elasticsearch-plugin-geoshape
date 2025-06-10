0785ac20306a0c4b20e04de047e3de6a9aac283f
Damien Garaud <damien.garaud@opendatasoft.com>
2025-06-10 10:48:03 +0200

    Remove the legacygeo lib from the package
    Just depending on jts.io, jts geom and elasticsearch.common.geo libs to handle
    the Geometry objects.

--- src/main/java/org/opendatasoft/elasticsearch/plugin/GeoUtils.java
+++ src/main/java/org/opendatasoft/elasticsearch/plugin/GeoUtils.java
@@ -7,6 +7,8 @@ import org.elasticsearch.common.hash.MurmurHash3;
 import org.elasticsearch.geometry.GeometryCollection;
 import org.elasticsearch.geometry.Line;
 import org.elasticsearch.geometry.LinearRing;
+import org.elasticsearch.geometry.MultiLine;
+import org.elasticsearch.geometry.MultiPoint;
 import org.elasticsearch.geometry.MultiPolygon;
 import org.elasticsearch.geometry.Point;
 import org.elasticsearch.geometry.Polygon;
@@ -17,6 +19,7 @@ import org.elasticsearch.legacygeo.builders.PolygonBuilder;
 import org.elasticsearch.legacygeo.builders.ShapeBuilder;
 import org.locationtech.jts.geom.Coordinate;
 import org.locationtech.jts.geom.Geometry;
+import org.locationtech.jts.geom.LineString;
 import org.locationtech.jts.io.ParseException;
 import org.locationtech.jts.io.WKBReader;
 import org.locationtech.jts.io.WKBWriter;
@@ -59,6 +62,131 @@ public class GeoUtils {
     public static GeoPoint getCentroidFromGeom(Geometry geom) {
         Geometry geom_centroid = geom.getCentroid();
         return new GeoPoint(geom_centroid.getCoordinate().y, geom_centroid.getCoordinate().x);
+    }
+
+    public static double getArea(org.elasticsearch.geometry.Geometry geom) {
+        Geometry jtsGeom = convertToJTS(geom);
+        return jtsGeom.getArea();
+    }
+
+    public static GeoPoint getCentroidFromGeom(org.elasticsearch.geometry.Geometry geom) {
+        Geometry jtsGeom = convertToJTS(geom);
+        org.locationtech.jts.geom.Point point = jtsGeom.getCentroid();
+        return new GeoPoint(point.getY(), point.getX());
+    }
+
+    public static Geometry getEnvelope(org.elasticsearch.geometry.Geometry geom) {
+        Geometry jtsGeom = convertToJTS(geom);
+        return jtsGeom.getEnvelope();
+    }
+
+    /**
+     * Convert Elasticsearch Geometry → JTS Geometry
+     */
+    public static org.locationtech.jts.geom.Geometry convertToJTS(org.elasticsearch.geometry.Geometry esGeometry) {
+        org.locationtech.jts.geom.GeometryFactory factory = new org.locationtech.jts.geom.GeometryFactory();
+
+        return switch (esGeometry.type()) {
+            case POINT -> {
+                Point esPoint = (Point) esGeometry;
+                yield factory.createPoint(new Coordinate(esPoint.getLon(), esPoint.getLat()));
+            }
+            case LINESTRING -> {
+                Line esLine = (Line) esGeometry;
+                Coordinate[] coords = extractCoordinates(esLine.getLons(), esLine.getLats());
+                yield factory.createLineString(coords);
+            }
+            case POLYGON -> {
+                Polygon esPolygon = (Polygon) esGeometry;
+                yield convertPolygonToJTS(esPolygon, factory);
+            }
+            case MULTIPOINT -> {
+                MultiPoint esMultiPoint = (MultiPoint) esGeometry;
+                Coordinate[] coordinates = new Coordinate[esMultiPoint.size()];
+                int index = 0;
+
+                for (org.elasticsearch.geometry.Geometry geom : esMultiPoint) {
+                    Point point = (Point) geom;
+                    coordinates[index++] = new Coordinate(point.getX(), point.getY());
+                }
+                yield factory.createMultiPointFromCoords(coordinates);
+            }
+            case MULTILINESTRING -> {
+                MultiLine esMultiLine = (MultiLine) esGeometry;
+                LineString[] lineStrings = new LineString[esMultiLine.size()];
+
+                int index = 0;
+                for (org.elasticsearch.geometry.Geometry geom : esMultiLine) {
+                    Line line = (Line) geom;
+                    Coordinate[] coords = extractCoordinates(line.getLons(), line.getLats());
+                    lineStrings[index++] = factory.createLineString(coords);
+                }
+                yield factory.createMultiLineString(lineStrings);
+            }
+            case MULTIPOLYGON -> {
+                MultiPolygon esMultiPolygon = (MultiPolygon) esGeometry;
+                yield convertMultiPolygonToJTS(esMultiPolygon, factory);
+            }
+            case GEOMETRYCOLLECTION -> {
+                @SuppressWarnings("unchecked")
+                GeometryCollection<org.elasticsearch.geometry.Geometry> esCollection = (GeometryCollection<
+                    org.elasticsearch.geometry.Geometry>) esGeometry;
+                yield convertGeometryCollectionToJTS(esCollection, factory);
+            }
+            default -> throw new IllegalArgumentException("Unsupported geometry type: " + esGeometry.type());
+        };
+    }
+
+    private static Coordinate[] extractCoordinates(double[] longs, double[] lats) {
+        Coordinate[] coords = new Coordinate[lats.length];
+        for (int i = 0; i < lats.length; i++) {
+            coords[i] = new Coordinate(longs[i], lats[i]);
+        }
+        return coords;
+    }
+
+    private static org.locationtech.jts.geom.Polygon convertPolygonToJTS(
+        Polygon esPolygon,
+        org.locationtech.jts.geom.GeometryFactory factory
+    ) {
+        // Anneau extérieur
+        LinearRing exterior = esPolygon.getPolygon();
+        Coordinate[] exteriorCoords = extractCoordinates(exterior.getLons(), exterior.getLats());
+        org.locationtech.jts.geom.LinearRing jtsExterior = factory.createLinearRing(exteriorCoords);
+
+        // Trous
+        org.locationtech.jts.geom.LinearRing[] jtsHoles = new org.locationtech.jts.geom.LinearRing[esPolygon.getNumberOfHoles()];
+        for (int i = 0; i < esPolygon.getNumberOfHoles(); i++) {
+            LinearRing hole = esPolygon.getHole(i);
+            Coordinate[] holeCoords = extractCoordinates(hole.getLons(), hole.getLats());
+            jtsHoles[i] = factory.createLinearRing(holeCoords);
+        }
+
+        return factory.createPolygon(jtsExterior, jtsHoles);
+    }
+
+    private static org.locationtech.jts.geom.MultiPolygon convertMultiPolygonToJTS(
+        MultiPolygon esMultiPolygon,
+        org.locationtech.jts.geom.GeometryFactory factory
+    ) {
+        org.locationtech.jts.geom.Polygon[] jtsPolygons = new org.locationtech.jts.geom.Polygon[esMultiPolygon.size()];
+        for (int i = 0; i < esMultiPolygon.size(); i++) {
+            jtsPolygons[i] = convertPolygonToJTS(esMultiPolygon.get(i), factory);
+        }
+        return factory.createMultiPolygon(jtsPolygons);
+    }
+
+    private static org.locationtech.jts.geom.GeometryCollection convertGeometryCollectionToJTS(
+        GeometryCollection<org.elasticsearch.geometry.Geometry> esCollection,
+        org.locationtech.jts.geom.GeometryFactory factory
+    ) {
+
+        org.locationtech.jts.geom.Geometry[] jtsGeometries = new org.locationtech.jts.geom.Geometry[esCollection.size()];
+        int i = 0;
+        for (org.elasticsearch.geometry.Geometry geometry : esCollection) {
+            jtsGeometries[i++] = convertToJTS(geometry);
+        }
+        return factory.createGeometryCollection(jtsGeometries);
     }
 
     // Return true if wkb is a point
